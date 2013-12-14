c7b400f2a37c5885ae6af263f880298ef439ceb2
dvance <dvance@ucar.edu>
2013-12-14 00:42:35 -0700

    Update plug-in so it works now. Added date/num photos to album

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,38 +1,50 @@
-BP_PLUGIN_PREFIX   = "/photos/TheBigPicture"
-PLUGIN_TITLE       = "The Big Picture"
-BP_RSS_FEED        = "http://www.boston.com/bigpicture/index.xml"
-NAMESPACES         = { "pheedo": "http://www.pheedo.com/namespace/pheedo" }
+BP_PLUGIN_PREFIX = "/photos/TheBigPicture"
+PLUGIN_TITLE = "The Big Picture"
+BP_RSS_FEED = "http://feeds.boston.com/boston/bigpicture/index"
+NAMESPACES = {"pheedo": "http://www.pheedo.com/namespace/pheedo"}
 
-ART         = "art-default.jpg"
-ICON        = "icon-default.png"
+ART = R("art-default.jpg")
+ICON = R("icon-default.png")
+
+NUM_PHOTOS_REGEX = Regex('(\d+) photos')
 
 ####################################################################################################
 def Start():
-  Plugin.AddPrefixHandler(BP_PLUGIN_PREFIX, MainMenu, PLUGIN_TITLE, ICON, ART)
-  Plugin.AddViewGroup("ImageStream", viewMode="Pictures", mediaType="items")
-  Plugin.AddViewGroup("List", viewMode="List", mediaType="items")
-  Plugin.AddViewGroup("InfoList", viewMode="InfoList", mediaType="items")
+    Plugin.AddViewGroup("ImageStream", viewMode="Pictures", mediaType="items")
+    Plugin.AddViewGroup("List", viewMode="List", mediaType="items")
+    Plugin.AddViewGroup("InfoList", viewMode="InfoList", mediaType="items")
 
-  ObjectContainer.art       = R(ART)
-  ObjectContainer.title1    = PLUGIN_TITLE
-  ObjectContainer.view_group = "InfoList"
-  DirectoryObject.thumb      = R(ICON)
+    ObjectContainer.art = ART
+    ObjectContainer.title1 = PLUGIN_TITLE
+    ObjectContainer.view_group = "InfoList"
+    DirectoryObject.thumb = ICON
+
 
 ####################################################################################################
+@handler(BP_PLUGIN_PREFIX, PLUGIN_TITLE)
 def MainMenu():
-  oc = ObjectContainer(view_group = "InfoList")
-  feed = XML.ElementFromURL(BP_RSS_FEED)
-  for item in feed.xpath("//rss//channel//item"):
-    description = item.xpath(".//description")[0].text.replace('&gt;','>').replace('&lt','<')
+    oc = ObjectContainer()
+    feed = XML.ElementFromURL(BP_RSS_FEED)
+    for item in feed.xpath("//rss//channel//item"):
+        description = item.xpath(".//description")[0].text.replace('&gt;', '>').replace('&lt', '<')
 
-    # If no thumb is provided, we can assume no photo is available. This has been found when a daily
-    # post was not made, actually due to illness. :(
-    try:
-      thumb = HTML.ElementFromString(description).xpath(".//div[@class='bpImageTop']//img")[0].get('src')
-    except: continue
+        # If no thumb is provided, we can assume no photo is available. This has been found when a daily
+        # post was not made, actually due to illness. :(
+        try:
+            thumb = HTML.ElementFromString(description).xpath(".//div[@class='bpImageTop']//img")[0].get('src')
+        except:
+            continue
 
-    url = item.xpath(".//pheedo:origLink", namespaces = NAMESPACES)[0].text
-    title = item.xpath(".//title")[0].text
-    oc.add(PhotoAlbumObject(url=url, title=title, thumb=thumb))
+        date = Datetime.ParseDate(item.xpath("./pubDate")[0].text)
+        url = item.xpath("./guid")[0].text
+        title = item.xpath("./title")[0].text
 
-  return oc
+        try:
+            num_photos = NUM_PHOTOS_REGEX.search(description).group(1)
+            summary = "%s - %s Photos" % (date.strftime("%x"), num_photos)
+        except:
+            summary = "%s" % date.strftime("%x")
+
+        oc.add(PhotoAlbumObject(url=url, title=title, thumb=thumb, originally_available_at=date, summary=summary))
+
+    return oc
